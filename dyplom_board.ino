7a3b6c87c61971910d1125e9b5ef3564c214cd54
kstamax <poberezhnyimax02@gmail.com>
2022-12-03 14:30:36 +0200

    NTP client added, logger sends timestamps

--- dyplom_board.ino
+++ dyplom_board.ino
@@ -1,15 +1,21 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-#define LED D0 
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#define LED D0
 #define GER D2
 #define RELAY D1
-// Replace with your network credentials
-const char* ssid     = "TP-Link_CC35";
+
+const char* ssid = "TP-Link_CC35";
 const char* password = "66433187";
 String serverName = "http://192.168.0.104:8000/gerkon/";
 
-// Set web server port number to 80
+const long utcOffsetInSeconds = 3600;
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
 WiFiServer server(80);
 
 // Variable to store the HTTP request
@@ -24,7 +30,7 @@ int prev_gerkon_read = LOW;
 // Current time
 unsigned long currentTime = millis();
 // Previous time
-unsigned long previousTime = 0; 
+unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
@@ -37,13 +43,16 @@ void setup() {
   prev_gerkon_read = digitalRead(GER);
   // Set outputs to LOW
   digitalWrite(LED, LOW);
-  digitalWrite(RELAY,LOW);
+  digitalWrite(RELAY, LOW);
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    digitalWrite(LED, LOW);
+    delay(250);
+    digitalWrite(LED, HIGH);
+    delay(250);
     Serial.print(".");
   }
   // Print local IP address and start web server
@@ -52,23 +61,24 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   server.begin();
+  timeClient.begin();
 }
 
-void loop(){
+void loop() {
   HTTPClient http;
-  WiFiClient client = server.available();   // Listen for incoming clients
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
+  WiFiClient client = server.available();  // Listen for incoming clients
+  if (client) {                            // If a new client connects,
+    Serial.println("New Client.");         // print a message out in the serial port
+    String currentLine = "";               // make a String to hold incoming data from the client
     currentTime = millis();
     previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();         
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {  // if there's bytes to read from the client,
+        char c = client.read();  // read a byte, then
+        Serial.write(c);         // print it out the serial monitor
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
+        if (c == '\n') {  // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
@@ -78,7 +88,7 @@ void loop(){
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-            
+
             // turns the GPIOs on and off
             if (header.indexOf("GET /0/off") >= 0) {
               Serial.println("GPIO 0 off");
@@ -88,48 +98,47 @@ void loop(){
               Serial.println("GPIO 0 on");
               output0State = "on";
               digitalWrite(LED, HIGH);
-            }  else if (header.indexOf("GET /2") >= 0) {
-              if (digitalRead(GER) == HIGH){
+            } else if (header.indexOf("GET /2") >= 0) {
+              if (digitalRead(GER) == HIGH) {
                 Serial.println("GPIO 1 HIGH");
                 output1State = "on";
                 digitalWrite(RELAY, HIGH);
-              }
-              else{
+              } else {
                 Serial.println("GPIO 1 LOW");
                 output1State = "off";
                 digitalWrite(RELAY, LOW);
               }
             }
-            
+
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
+            // CSS to style the on/off buttons
             // Feel free to change the background-color and font-size attributes to fit your preferences
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
             client.println(".button2 {background-color: #77878A;}</style></head>");
-            
+
             // Web Page Heading
             client.println("<body><h1>ESP8266 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 5  
+
+            // Display current state, and ON/OFF buttons for GPIO 5
             client.println("<p>GPIO 0 - State " + output0State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output0State=="off") {
+            // If the output5State is off, it displays the ON button
+            if (output0State == "off") {
               client.println("<p><a href=\"/0/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/0/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
+            }
             client.println("</body></html>");
-            
+
             // The HTTP response ends with another blank line
             client.println();
             // Break out of the while loop
             break;
-          } else { // if you got a newline, then clear currentLine
+          } else {  // if you got a newline, then clear currentLine
             currentLine = "";
           }
         } else if (c != '\r') {  // if you got anything else but a carriage return character,
@@ -145,20 +154,24 @@ void loop(){
     Serial.println("");
   }
   int gerkon_read = digitalRead(GER);
-  if (gerkon_read != prev_gerkon_read){
-    if (gerkon_read == HIGH){
-      String serverPath = serverName + "1";
+  int httpResponseCode = -1;
+  if (gerkon_read != prev_gerkon_read) {
+    timeClient.update();
+    if (gerkon_read == HIGH) {
+      String serverPath = serverName + "1/" + timeClient.getFormattedTime();
       http.begin(client, serverPath.c_str());
-      int httpResponseCode = http.GET();
+      httpResponseCode = http.GET();
+      Serial.println(httpResponseCode);
       digitalWrite(LED, HIGH);
-    }
-    else{
-      String serverPath = serverName + "0";
+    } else {
+      String serverPath = serverName + "0/" + timeClient.getFormattedTime();
       http.begin(client, serverPath.c_str());
-      int httpResponseCode = http.GET();
+      httpResponseCode = http.GET();
+      Serial.println(httpResponseCode);
       digitalWrite(LED, LOW);
+    }
+    if (httpResponseCode == -1) {
     }
     prev_gerkon_read = gerkon_read;
   }
-  
 }
